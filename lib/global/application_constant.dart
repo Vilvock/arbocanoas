6e4fa8eccfd0e3e78619fe9c5f9bfe5b148151f4
Vilvock <64989193+Vilvock@users.noreply.github.com>
2023-07-24 16:36:12 -0300

    CONFIG

--- lib/global/application_constant.dart
+++ lib/global/application_constant.dart
@@ -3,22 +3,12 @@ class ApplicationConstant {
   static const String FCM_TYPE_ANDROID = "1";
   static const String FCM_TYPE_IOS = "2";
 
-
   static const String URL_VIA_CEP = "https://viacep.com.br/ws/";
 
   static const String URL = "https://icontrolssd.com.br/";
-  static const String URL_BASE = "https://icontrolssd.com.br/apiv3/user/";
-  static const String TOKEN = "12Qge8d3";
+  static const String URL_BASE = "https://www.arbocanoas.com.br/apiv3/user/";
+  static const String TOKEN = "656khHiv";
 
-
-  static const int CREDIT_CARD = 1;
-  static const int TICKET = 2;
-  static const int PIX = 3;
-  static const int TICKET_IN_TERM = 4;
-
-  static const String URL_AVATAR = URL + "uploads/avatar/";
-  static const String URL_ATTACHMENTS = URL + "uploads/anexos/";
-  static const String URL_FLEETS = URL + "uploads/frotas/";
 
 
 }
